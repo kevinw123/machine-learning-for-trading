1bf7aacb8d1254d2f79a1bdbaea8fd1c34869e7d
Kevin Wong <kevinwong_895@hotmail.com>
2018-02-17 10:07:07 -0800

    minimize function

--- project1/optimize.py
+++ project1/optimize.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import pandas as pd
+import numpy as np
+import scipy.optimize as spo
 
 # Return CSV file path given a stock symbol.
 def symbol_to_path(symbol, base_dir="data"):
@@ -21,12 +23,37 @@ def get_data(symbols, dates, SPY=True):
 
     return df
 
+# Helper function for minimize
+def error_optimal_allocations(allocs, prices):
+    port_val = get_portfolio_values(prices, allocs, 1)
+    cum_ret, avg_daily_ret, std_daily_ret, sharpe_ratio = get_portfolio_stats(port_val)
+    error = sharpe_ratio * -1
+    return error
 
+# Finds optimal allocation for stock price, optimizing for Sharpe ratio
+def find_optimal_allocation(portfolio_prices):
+    guess = 1.0/portfolio_prices.shapes[1]
+    function_guess = [guess] * portfolio_prices.shape[1]
+    bnds = [[0,1] for _ in portfolio_prices.columns]
+    cons = ({ "type" : "eq", "fun": lambda function_guess: 1.0 - np.sum(function_guess) })
+    result = spo.minimum(error_optimal_allocations, function_guess, args = (portfolio_prices,), methods="SLSQP", bounds = bnds, constraints = cons, options={'disp':True})
+    allocs = result.x
+    return allocs
+
+def run_optimization(symbols, start_date, end_date):
+    dates = pd.date_range(start_date, end_date)
+    all_prices = get_data(symbols, dates)
+
+    portfolio_prices = all_prices[symbols]
+    spy_prices = all_prices["SPY"]
+
+    allocations = find_optimal_allocation(portfolio_prices)
+    allocations = allocations / np.sum(allocations) # normalize allocations
 
 # Problem is, given a set of stocks, start and end date, maximize by Sharpe Ratio
 def run_simulation():
-    startDate = "2010-02-01"
-    endDate = "2012-02-01"
+    start_date = "2010-02-01"
+    end_date = "2012-02-01"
 
     symbols = ["AAPL","MSFT","YHOO","GOOG"]
 
@@ -43,7 +70,8 @@ def run_simulation():
 
             symbols.append(sys.argv[i])
 
-        print(symbols)
+
+    run_optimization(symbols, start_date, end_date)
 
 if __name__ == "__main__":
     run_simulation()
